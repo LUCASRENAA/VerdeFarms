ff81f14658ede362f28367ac35b5038df247f9f5
LUCASRENAA_IFPE <lrms2@discente.ifpe.edu.br>
2022-12-22 17:42:38 -0300

    timedelta token

--- front/views.py
+++ front/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render
+from core.models import Vendedor,Geolocalizacao,Fazenda,FazendaVendedor,Produto,ProdutoVendedor,FavoritoVendedor,FavoritoProduto,Feira
 
 def index(request):
-    return render(request, "index.html")
+    geo = Geolocalizacao.objects.all()
+    dados = {"geo":geo}
+    return render(request, "index2.html",dados)
