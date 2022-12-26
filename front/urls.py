8d4bc1ea9202c199dfa203c092577346b8bc6b51
LUCASRENAA_IFPE <lrms2@discente.ifpe.edu.br>
2022-12-26 10:28:55 -0300

    auth-json

--- front/urls.py
+++ front/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("login", views.login_user),
+    path("registro", views.registro),
+    path("submit", views.submit_login),
+    path("submit/registro", views.submit_registro),
+
 ]
